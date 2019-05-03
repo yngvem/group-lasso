1a91c1c135afc73f60fdb5e5807fa57274cee5aa
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2019-05-03 08:04:19 +0200

    Restructured code by moving stuff out of class

--- group_lasso_fista.py
+++ group_lasso_fista.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from numbers import Number
 import warnings
 
 import numpy.linalg as la
@@ -9,25 +10,34 @@ LIPSCHITZ_MAXITS = 50
 LIPSCHITS_TOL = 1e-2
 
 
-def _subsample(rate, *Xs):
-    num_rows = len(Xs[0])
-    subsampled_rows = int(num_rows*rate)
-    if rate < 1:
-        inds = np.random.choice(len(X), subsampled_rows, replace=False)
-        inds.sort()
+def _extract_from_singleton_tuple(inputs):
+    if len(inputs) == 1:
+        return inputs[0]
+    return inputs
 
-        if len(Xs) == 1:
-            return Xs[0][inds, :]
-        return tuple([X[inds, :] for X in Xs])
-    else:
-        if len(Xs) == 1:
-            return Xs[0]
-        return Xs
+
+def _subsample(rate, *Xs):
+    """Subsample along first (0-th) axis of the Xs arrays.
+    """
+    assert len(Xs) > 0
+    assert rate <= 1
+
+    if rate == 1:
+        return _extract_from_singleton_tuple(Xs)
+
+    num_rows = len(Xs[0])
+    num_subsampled_rows = int(num_rows*rate)
+    inds = np.random.choice(len(X), num_subsampled_rows, replace=False)
+    inds.sort()
+
+    return _extract_from_singleton_tuple(tuple([X[inds, :] for X in Xs]))
 
 
 def _find_largest_singular_value(
     X, subsampling_rate=1, maxits=LIPSCHITZ_MAXITS, tol=LIPSCHITS_TOL
 ):
+    """Find the largest singular value of X.
+    """
     v = np.random.randn(X.shape[1], 1)
     s = la.norm(v)
     v /= s
@@ -40,10 +50,7 @@ def _find_largest_singular_value(
         v /= s_
         s_ /= subsampling_rate
 
-        # The absolute value signs should be unnecessary
-        # I think the singular value approximation will
-        # converge monotonically, but I'll add them for
-        # safety.
+        # Absolute value is necessary because of subsampling
         improvement = abs(s_ - s)/max(abs(s_), abs(s))
         s = s_
         if improvement < tol and i > 0:
@@ -57,7 +64,8 @@ def _find_largest_singular_value(
     warnings.warn(
         f'Could not find an estimate for the largest singular value of X'
         f'with the power method. \n'
-        f'Ran for {maxits:d} iterations with a tolerance of {tol:02g}',
+        f'Ran for {maxits:d} iterations with a tolerance of {tol:02g}'
+        f'Subsampling {"is" if subsampling_rate != 1 else "is not"} used.',
         RuntimeWarning
     )
     return s
@@ -69,13 +77,30 @@ def _l2_prox(w, reg):
     return max(0, 1 - reg/la.norm(w))*w
 
 
+def _l2_grad(A, x, b):
+    return A.T@(A@x - b)
+
+
+def _subsampled_l2_grad(A, x, b, rate):
+    A, b = _subsample(rate, A, b)
+    return _l2_grad(A, x, b)
+
+
+def _group_l2_prox(w, reg_coeffs, groups):
+    """The proximal map for the specified coefficients.
+    """
+    w = w.copy()
+    for (start, end), reg in zip(groups, reg_coeffs):
+        reg = reg*sqrt(end - start)
+        w[start:end, :] = _l2_prox(w[start:end, :], reg)
+    return w
+
+
 class GroupLassoRegressor:
     """
     This class implements the Group Lasso [1] penalty for linear regression.
     The loss is optimised using the FISTA algorithm proposed in [2] with the
     generalised gradient-based restarting scheme proposed in [3].
-
-
 
 
     [1]: Yuan M, Lin Y. Model selection and estimation in regression with
@@ -128,17 +153,21 @@ class GroupLassoRegressor:
         """
         self.groups = groups
         self._reset_groups = False
-        self.reg = reg
+        self.reg = self._get_reg_vector(reg)
         self.n_iter = n_iter
         self.tol = tol
         self.subsampling_rate = subsampling_rate
 
     def _regularizer(self, w):
         regularizer = 0
-        for start, end in self.groups:
-            reg = self.reg*sqrt(end - start)
+        for (start, end), reg in zip(self.groups, self.reg):
             regularizer += reg*la.norm(w[start:end, :])
         return regularizer
+
+    def _get_reg_vector(self, reg):
+        if isinstance(reg, Number):
+            return [reg*sqrt(end - start) for start, end in self.groups]
+        return reg
 
     def _loss(self, X, y, w):
         MSE = np.sum((X@w - y)**2)/len(X)
@@ -169,25 +198,18 @@ class GroupLassoRegressor:
     def _fista(self, X, y, lipschitz_coef=None):
         """Use the FISTA algorithm to solve the group lasso regularised loss.
         """
+        num_rows, num_cols = X.shape
+
         if lipschitz_coef is None:
             lipschitz_coef = _find_largest_singular_value(
                 X, subsampling_rate=self.subsampling_rate
-            )*1.1/len(X)
+            )*1.1/num_rows
 
         def grad(w):
-            """A sampled approximation to the gradient of the MSE loss
-            """
-            X_, y_ = _subsample(self.subsampling_rate, X, y)
-            return X_.T@(X_@w - y_)/len(X_)
+            return _subsampled_l2_grad(X, w, y, self.subsampling_rate)/num_rows
 
         def prox(w):
-            """The proximal map for the specified coefficients.
-            """
-            w = w.copy()
-            for start, end in self.groups:
-                reg = self.reg*sqrt(end - start)
-                w[start:end, :] = _l2_prox(w[start:end, :], reg)
-            return w
+            return _group_l2_prox(w, self.reg, self.groups)
 
         u = self.coef_
         v = self.coef_
@@ -197,7 +219,6 @@ class GroupLassoRegressor:
             X_, y_ = _subsample(self.subsampling_rate, X, y)
             print(f'Starting FISTA: ')
             print(f'\tInitial loss: {self.loss(X_, y_)}')
-
 
         for i in range(self.n_iter):
             u_, v, t = self._fista_it(u, v, t, lipschitz_coef, grad, prox)
@@ -231,12 +252,12 @@ class GroupLassoRegressor:
         if self.groups is None or self._reset_groups:
             self._reset_groups = True
             self.groups = [(i, i+1) for i, _ in range(X.shape[1])]
-        
+
         for group1, group2 in zip(self.groups[:-1], self.groups[1:]):
             assert group1[0] < group1[1]
             assert group1[1] <= group2[0]
 
-        assert self.reg >= 0
+        assert all(reg > 0 for reg in self.reg)
         assert self.n_iter > 0
         assert self.tol > 0
         assert self.subsampling_rate > 0 and self.subsampling_rate <= 1
@@ -252,10 +273,10 @@ class GroupLassoRegressor:
     def fit(self, X, y, lipschitz_coef=None):
         self._init_fit(X, y)
         self._fista(X, y, lipschitz_coef=lipschitz_coef)
-    
+
     def predict(self, X):
         return X@self.coef_
-    
+
     def fit_predict(self, X, y):
         self.fit(X, y)
         return self.predict(X)
@@ -290,7 +311,7 @@ if __name__ == '__main__':
     group_sizes = [np.random.randint(3, 10) for i in range(50)]
     groups = get_groups_from_group_sizes(group_sizes)
     num_coeffs = sum(group_sizes)
-    num_datapoints = 10000000
+    num_datapoints = 10000
     noise_level = 0.5
 
     print('Generating data')
@@ -303,7 +324,7 @@ if __name__ == '__main__':
     y += np.random.randn(*y.shape)*noise_level*y
 
     gl = GroupLassoRegressor(
-        groups=groups, n_iter=50, tol=0.1, reg=0.1, subsampling_rate=0.001
+        groups=groups, n_iter=50, tol=0.1, reg=0.01, subsampling_rate=1
     )
     print('Starting fit')
     gl.fit(X, y)
