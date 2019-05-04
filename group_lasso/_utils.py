206ea537e2e2ca1dcface36e56e79e0c11072fcb
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2019-05-04 18:48:19 +0200

    Removed noise on generated components

--- group_lasso/_utils.py
+++ group_lasso/_utils.py
@@ -4,14 +4,12 @@ import numpy as np
 def generate_group_lasso_coefficients(
     group_sizes,
     inclusion_probability=0.5,
-    included_std=1,
-    noise_level=0,
+    coeff_std=1,
 ):
     coefficients = []
     for group_size in group_sizes:
         coefficients_ = np.random.randn(group_size, 1)*included_std
         coefficients_ *= (np.random.uniform(0, 1) < inclusion_probability)
-        coefficients_ += np.random.randn(group_size, 1)*noise_level
         coefficients.append(coefficients_)
 
     return np.concatenate(coefficients, axis=0)
