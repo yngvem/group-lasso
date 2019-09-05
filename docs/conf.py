bac4bffe1fd76041aeca246136ce3438387020d4
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2019-09-05 13:10:11 +0200

    Bump version: 1.1.0 → 1.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Yngve Mardal Moe"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "1.1.0"
+release = "1.1.1"
 
 
 # -- General configuration ---------------------------------------------------
--- src/group_lasso/__init__.py
+++ src/group_lasso/__init__.py
@@ -2,7 +2,7 @@
 """
 
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __author__ = "Yngve Mardal Moe"
 __email__ = "yngve.m.moe@gmail.com"
 
