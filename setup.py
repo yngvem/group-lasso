e2fd89bc38452d3cd22c1c1ca439366301279d3b
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2019-09-05 11:01:46 +0200

    Fixed so setup.cfg takes care of all config

--- setup.py
+++ setup.py
@@ -7,18 +7,4 @@ with open("README.rst") as f:
 
 
 setup(
-    name="group-lasso",
-    version="1.0.0",
-    license="MIT",
-    description=(
-        "Fast group lasso regularised linear models in a sklearn-style API."
-    ),
-    long_description=long_description,
-    author="Yngve Mardal Moe",
-    author_email="yngve.m.moe@gmail.com",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-    install_requires=["numpy", "scikit-learn"],
-    extras_require={"test": ["pytest"]},
 )
