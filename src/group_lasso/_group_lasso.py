ee826c0c8c3ffff4c7f0030cea49d831f145391b
Norman <normanius@gmail.com>
2020-04-26 19:38:28 +0200

    Fix the previous edit - self.groups must not be modified when fit() is called; as a side effect, this might fix issue #yngvem/group-lasso#13

--- src/group_lasso/_group_lasso.py
+++ src/group_lasso/_group_lasso.py
@@ -366,9 +366,7 @@ class BaseGroupLasso(ABC, BaseEstimator, TransformerMixin):
         """
         assert all(reg >= 0 for reg in self.group_reg_vector_)
         groups = np.array(self.groups)
-        assert len(self.group_reg_vector_) == len(
-            np.unique(groups[groups >= 0])
-        )
+        assert len(self.group_reg_vector_) == len(self.groups_)
         assert self.n_iter > 0
         assert self.tol >= 0
 
@@ -398,15 +396,18 @@ class BaseGroupLasso(ABC, BaseEstimator, TransformerMixin):
     def _init_fit(self, X, y, lipschitz):
         """Initialise model and check inputs.
         """
-        if self.groups is None:
+        groups = self.groups
+        if groups is None:
             # TODO: possibly drop a warning that group-lasso is not
             # functional if no groups are specified.
-            self.groups = np.arange(X.shape[1])
+            groups = np.arange(X.shape[1])
+        self.groups_init_ = groups.ravel() # Validated user setting
+        groups = np.array([-1 if i is None else i for i in groups])
+
         self.random_state_ = check_random_state(self.random_state)
         X, y, lipschitz = self._prepare_dataset(X, y, lipschitz)
-        groups = np.array([-1 if i is None else i for i in self.groups])
 
-        self.groups_ = [self.groups == u for u in np.unique(groups) if u >= 0]
+        self.groups_ = [groups == u for u in np.unique(groups) if u >= 0]
         self.group_reg_vector_ = self._get_reg_vector(self.group_reg)
         self.losses_ = []
 
@@ -466,10 +467,9 @@ class BaseGroupLasso(ABC, BaseEstimator, TransformerMixin):
 
     @property
     def chosen_groups_(self):
-        """A set of the coosen group ids.
+        """A set of the chosen group ids.
         """
-        sparsity_mask = self._get_chosen_coef_mask(self.coef_)
-        return set(np.unique(self.groups.ravel()[sparsity_mask.ravel()]))
+        return set(self.groups_init_[self.sparsity_mask_.ravel()])
 
     def transform(self, X):
         """Remove columns corresponding to zero-valued coefficients.
