1049c67ae5231f0267427a7ef555de11d24b6aae
Kris Stern <krisstern@outlook.com>
2023-07-16 23:12:07 +0800

    Update build instructions

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import find_packages, setup
 
-with open("README.rst") as f:
-    long_description = f.read()
-
-
-setup()
+setup(name="group-lasso",
+      version="1.5.0",
+      description="Fast group lasso regularised linear models in a sklearn-style API",
+      author="Yngve Mardal Moe",
+      author_email="yngve.m.moe@gmail.com",
+      url="https://group-lasso.readthedocs.io/en/latest/",
+      packages=find_packages(
+            where="src",
+            include=["group_lasso*"]
+      ),
+      package_dir={"": "src"},
+      include_package_data=True
+      )
